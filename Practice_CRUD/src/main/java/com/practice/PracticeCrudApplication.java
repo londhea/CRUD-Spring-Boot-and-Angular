3999cc58b73e55b868c7a1b7f5042c904ce9550d
londhea <49476313+londhea@users.noreply.github.com>
2021-05-20 13:32:26 +0530

    Created a Spring boot starter project

---
+++ Practice_CRUD/src/main/java/com/practice/PracticeCrudApplication.java
@@ -0,0 +1,13 @@
+package com.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeCrudApplication.class, args);
+	}
+
+}
